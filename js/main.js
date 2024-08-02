f999daa7751fea999eda570128bb4e406071a8a4
kelwin <kelwin27@gmail.com>
2024-08-03 00:20:22 +0300

    feat: add content edit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 import '../css/style.css';
 import '@fontsource/poppins';
 import '@fontsource/poppins/500.css';
+
+document.body.addEventListener('click', function (event) {
+  const currentEditable = document.querySelector('[contenteditable="true"]');
+  if (currentEditable && currentEditable !== event.target) {
+    currentEditable.removeAttribute('contenteditable');
+    localStorage.setItem(
+      currentEditable.className,
+      currentEditable.textContent
+    );
+  }
+
+  if (event.target.classList.contains('edit')) {
+    event.target.setAttribute('contenteditable', 'true');
+    event.target.focus();
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.edit').forEach(function (editable) {
+    const savedContent = localStorage.getItem(editable.className);
+    if (savedContent) {
+      editable.textContent = savedContent;
+    }
+  });
+});
